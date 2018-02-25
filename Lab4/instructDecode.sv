a9d114b200afbd11c78568e0f5249e0d50c07aaa
carrot124 <33817437+carrot124@users.noreply.github.com>
2018-02-24 17:59:30 -0800

    Updated instructFetch and instructDecode

--- Lab4/instructDecode.sv
+++ Lab4/instructDecode.sv
@@ -1,23 +1,32 @@
-module instructDecode(clk, reset, ID_instruction, IFID_Rn, IFID_Rm, IFID_Rd, RegA_content, RegB_content, WriteData_fromWB, RegWrite_fromMEMWB, Reg2Loc, Imm12Extended, DAddr9Extended);
+module instructDecode(clk, reset, ID_instruction, IFID_Rn, IFID_Rm, IFID_Rd, RegA_content, RegB_content, WriteData_fromWB, RegWrite_fromMEMWB, Reg2Loc, Imm12Extended, DAddr9Extended
+							, UncondMuxOut, UncondBr);
 	input logic clk, reset;
 	input logic [31:0] ID_instruction;
 	input logic [63:0] WriteData_fromWB;
-	input logic RegWrite_fromMEMWB, Reg2Loc;
+	input logic RegWrite_fromMEMWB, Reg2Loc, UncondBr;
 	output logic [4:0] IFID_Rn, IFID_Rm, IFID_Rd;
 	output logic [63:0] RegA_content, RegB_content;
 	output logic [63:0] Imm12Extended;
 	output logic [63:0] DAddr9Extended;
+	output logic [63:0] UncondMuxOut;
 	
 // Assign statements for readability
 assign IFID_Rn = ID_instruction[9:5];
 assign IFID_Rm = ID_instruction[20:16];
 assign IFID_Rd = ID_instruction[4:0];
 
+
 logic imm12[11:0];
 assign imm12 = ID_instruction[21:10];
 
 logic dAddr9[8:0];
 assign dAddr9 = ID_instruction[20:12];
+
+logic condAddr19[18:0];
+assign condAddr19 = ID_instruction[23:5];
+
+logic brAddr26[25:0];
+assign brAddr26 = ID_instruction[25:0];
 
 // Reg2Loc Mux
 logic [4:0] Ab;
@@ -39,5 +48,18 @@ regfile theRegisterFile (.ReadData1(RegA_content), .ReadData2(RegB_content), .Wr
 	//Sign extend for DAddr9
 	// DAddr9Extended goes to ID/EX Pipe for next stage
 	signExtend #(.WIDTH(9)) DAddr9(.in(dAddr9), .out(DAddr9Extended));
+	
+// Sign Extend condAddr19 and brAddr26
+signExtend #(.WIDTH(19)) condAddrExtend (.in(condAddr19), .out(condAddrExtended));
+signExtend  #(.WIDTH(26)) brAddrExtend (.in(brAddr26), .out(brAddrExtended));
+
+	// Put results after sign extension into 2:1 Mux with control signal UncondBr
+	logic [63:0] condAddrExtended, brAddrExtended;
+	genvar i;
+	generate 
+		for (i = 0; i < 64; i++) begin: eachUncondMux
+			mux_2to1 theMuxes1 (.select(UncondBr), .a(condAddrExtended[i]), .b(brAddrExtended[i]), .y(UncondMuxOut[i]));
+		end
+	endgenerate
 	
 endmodule
--- Lab4/instructFetch.sv
+++ Lab4/instructFetch.sv
@@ -9,8 +9,9 @@ Instruction Fetch Stage Pipelined for CPU
 	
 *********************************************/
 `timescale 1ns/10ps
-module instructFetch (instruction, currentPC, UncondBr, BrTaken, clk, reset);
-	input logic BrTaken, UncondBr, clk, reset;
+module instructFetch (instruction, currentPC, BranchToAddress, BrTaken, clk, reset);
+	input logic BrTaken, clk, reset;
+	input logic [63:0] BranchToAddress;
 	output logic [31:0] instruction;
 	output logic [63:0] currentPC;
 	
@@ -18,36 +19,35 @@ module instructFetch (instruction, currentPC, UncondBr, BrTaken, clk, reset);
 	instructmem fecthInstruct(.address(currentPC), .instruction, .clk);
 	
 	logic [63:0] nextPC;
-	// Add 4 to current instructrion
+	// Add 4 to current instructrion (PC = PC+4)
 	addressAdder theAddressAdder1 (.A(currentPC), .B(64'd4), .sum(nextPC));
 	
-	logic [63:0] condAddrExtended, brAddrExtended;
-	// Sign Extend condAddr19 and brAddr26
-	signExtend #(.WIDTH(19)) condAddrExtend (.in(instruction[23:5]), .out(condAddrExtended));
-	signExtend  #(.WIDTH(26)) brAddrExtend (.in(instruction[25:0]), .out(brAddrExtended));
+	//logic [63:0] condAddrExtended, brAddrExtended;
+
 	
-	// Put results after sign extension into 2:1 Mux with control signal UncondBr
-	logic [63:0] UncondMuxOut;
-	genvar i;
-	generate 
-		for (i = 0; i < 64; i++) begin: eachUncondMux
-			mux_2to1 theMuxes1 (.select(UncondBr), .a(condAddrExtended[i]), .b(brAddrExtended[i]), .y(UncondMuxOut[i]));
-		end
-	endgenerate
-	
-	logic [63:0] shifterOut;
-	
-	// Shift result from UncondMux
-	shifter shifted (.value(UncondMuxOut), .direction(1'b0), .distance(6'b000010), .result(shifterOut));
-	logic [63:0] PCplusBranch;
-	// Add the shifted result with current PC
-	addressAdder theAddressAdder2 (.A(currentPC), .B(shifterOut), .sum(PCplusBranch));
+//	// Put results after sign extension into 2:1 Mux with control signal UncondBr
+//	logic [63:0] UncondMuxOut;
+//	genvar i;
+//	generate 
+//		for (i = 0; i < 64; i++) begin: eachUncondMux
+//			mux_2to1 theMuxes1 (.select(UncondBr), .a(condAddrExtended[i]), .b(brAddrExtended[i]), .y(UncondMuxOut[i]));
+//		end
+//	endgenerate
+//	
+//	logic [63:0] shifterOut;
+//	
+//	// Shift result from UncondMux
+//	shifter shifted (.value(UncondMuxOut), .direction(1'b0), .distance(6'b000010), .result(shifterOut));
+//	logic [63:0] PCplusBranch;
+//	// Add the shifted result with current PC
+//	addressAdder theAddressAdder2 (.A(currentPC), .B(shifterOut), .sum(PCplusBranch));
 	
 	// Mux to choose whether to branch or not
+	genvar i;
 	logic [63:0] BrTakenOut;
 	generate 
 		for (i = 0; i < 64; i++) begin: eachBrTakenMux
-			mux_2to1 theMuxes2 (.select(BrTaken), .a(nextPC[i]), .b(PCplusBranch[i]), .y(BrTakenOut[i]));
+			mux_2to1 theMuxes2 (.select(BrTaken), .a(nextPC[i]), .b(BranchToAddress[i]), .y(BrTakenOut[i]));
 		end
 	endgenerate
 	
@@ -57,11 +57,12 @@ module instructFetch (instruction, currentPC, UncondBr, BrTaken, clk, reset);
 endmodule
 
 module instructFetch_testbench;
-	logic clk, reset, BrTaken, UncondBr;
+	logic BrTaken, clk, reset;
+	logic [63:0] BranchToAddress;
 	logic [31:0] instruction;
 	logic [63:0] currentPC;
 
-	instructFetch dut(.instruction, .currentPC, .UncondBr, .BrTaken, .clk, .reset);
+	instructFetch dut(.instruction, .currentPC, .BranchToAddress, .BrTaken, .clk, .reset);
 
 	parameter CLOCK_PERIOD= 10000;
 	initial begin
@@ -71,11 +72,9 @@ module instructFetch_testbench;
 	integer i;
 	always_comb begin
 		if(instruction[31:26] == 6'b000101) begin
-			UncondBr = 1'b1;
 			BrTaken = 1'b1;
 		end
 		else begin
-			UncondBr = 1'b0;
 			BrTaken = 1'b0;
 		end
 	end
